bb729f7e6285b73d67ab866164ddd1a9834e7278
Linoy Baliti <linoy.baliti@post.runi.ac.il>
2025-12-30 12:19:17 +0200

    hw7-33

--- Binomial.java
+++ Binomial.java
@@ -18,36 +18,55 @@ public class Binomial {
 	 }
 	
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
-		if (k>n){
-			return 0;
-		}
-		int [][] memo = new int [n+1][k+1];
-		for (int i=0; i<memo.length; i++){
-			for (int j=0; j<memo[0].length; j++){
-				memo  [i][j] = -1;
-			}
-		}
-		return binomial(n, k, memo);
-	}
+    public static long binomial(int n, int k) {
+        // מקרה קצה: אם k גדול מ-n, התוצאה היא תמיד 0
+        if (k > n) {
+            return 0;
+        }
+        if (k < 0) {
+            return 0;
+        }
 
-	private static int binomial(int n, int k, int[][] memo) {
-		if (memo[n][k] != -1) {
-			return memo[n][k];
-		}
-		// Base case
-		if ((k > n)) {
-		   	memo[n][k] = 0; 
-		   	return 0;
-		}
-		// Another base case
-		if (n == 0 || k == 0) {
-		   	memo[n][k] = 1; 
-		   	return 1;
-		}
-		memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
-		return memo[n][k];
-	}
+        // יצירת מערך מסוג long כדי להכיל מספרים ענקיים
+        long[][] memo = new long[n + 1][k + 1];
+
+        // אתחול המערך בערך -1 לסימון שטרם חושב
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                memo[i][j] = -1;
+            }
+        }
+
+        return binomial(n, k, memo);
+    }
+
+    private static long binomial(int n, int k, long[][] memo) {
+        // בדיקה אם כבר חישבנו את הערך הזה בעבר
+        if (memo[n][k] != -1) {
+            return memo[n][k];
+        }
+
+        // מקרי בסיס
+        if (k == 0 || k == n) {
+            memo[n][k] = 1;
+            return 1;
+        }
+        
+        // הגנה נוספת למקרה ש-k גדול מ-n ברקורסיה
+        if (k > n) {
+            memo[n][k] = 0;
+            return 0;
+        }
+
+        // החישוב הרקורסיבי - זהות פסקל
+        memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
+        
+        return memo[n][k];
+    }
+
+
+
+
 }
 
 

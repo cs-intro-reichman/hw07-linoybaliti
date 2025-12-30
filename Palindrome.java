12e46b8316e32dac8829f820acb25be15d08b367
Linoy Baliti <linoy.baliti@post.runi.ac.il>
2025-12-30 12:09:54 +0200

    hw7-1

--- Palindrome.java
+++ Palindrome.java
@@ -8,6 +8,14 @@ public class Palindrome {
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
 		//// Replace the followaing statement with your code
-		return false;
-    }
+		if (s.length() == 0 || s.length() == 1 ){
+			return true;
+		}	
+		else if (s.charAt(0) != s.charAt(s.length()-1)){
+			return false;
+		}
+		
+		return isPalindrome(s.substring(1,s.length()-1));
+	}
+
 }
--- Sierpinski.java
+++ Sierpinski.java
@@ -7,12 +7,47 @@ public class Sierpinski {
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
+		double x1 = 0.0;
+		double y1 = 0.0;
+
+		double x2 = 1.0;
+		double y2 = 0.0;
+
+		double x3 = 0.5;
+		double y3 = Math.sqrt(3) / 2;
+
 		//// Replace this comment with your code
+		 sierpinski(n,x1,x2,x3,y1,y2,y3);
 	}
 	
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
 		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+	if (n == 0){
+		return;
 	}
+		StdDraw.line(x1, y1, x2, y2); // בסיס
+		StdDraw.line(x2, y2, x3, y3); // צלע ימין
+		StdDraw.line(x3, y3, x1, y1); // צלע שמאל
+
+		// חישוב נקודות האמצע של הצלעות
+        double x12 = (x1 + x2) / 2.0;
+        double y12 = (y1 + y2) / 2.0;
+
+        double x23 = (x2 + x3) / 2.0;
+        double y23 = (y2 + y3) / 2.0;
+
+        double x31 = (x3 + x1) / 2.0;
+        double y31 = (y3 + y1) / 2.0;
+
+        // שלוש קריאות רקורסיביות עבור שלושת המשולשים הקטנים בפינות
+        sierpinski(n - 1, x1, x12, x31, y1, y12, y31);  // משולש שמאלי
+        sierpinski(n - 1, x12, x2, x23, y12, y2, y23);  // משולש ימני
+        sierpinski(n - 1, x31, x23, x3, y31, y23, y3);  // משולש עליון
+    }
+
+
+	
+
+	
 }
